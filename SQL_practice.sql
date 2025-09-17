5ced2cc33c3115cd3cad8d69295d09507ea55802
diptip10 <diptipawar811@gmail.com>
2025-09-17 23:34:20 +0530

    cte

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -1,12 +1,14 @@
 -- 1. Find the second highest salary from the salaries table. 
 
 -- 1) by using subquery
+
 SELECT max(salary) as sec_high_sal from SALARIES 
 where salary<(
     SELECT max(salary) from salaries
 );
 
 -- 2) by using limit and offset
+
 select DISTINCT salary from salaries
 ORDER BY salary DESC
 limit 1 offset 1;
@@ -18,16 +20,10 @@ SELECT salary FROM(
 ) as rank where rank =2;
 
 -- 4) with cte
-WITH salary_ranks AS (
-    SELECT 
-        s.*,
-        DENSE_RANK() OVER (ORDER BY s.salary DESC) AS rnk
-    FROM salaries s
-)
-SELECT *
-FROM salary_ranks
-WHERE rnk = 2;
 
-
-
-
+with salary_rank as(
+    select 
+    s.*, DENSE_RANK() OVER (order by s.salary DESC) rnk 
+    from salaries s
+) select * from salary_rank 
+where rnk = 2 ;
