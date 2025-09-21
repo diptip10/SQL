fcab33229dc4ed02893951cb0b52ceccf93fcf50
diptip10 <diptipawar811@gmail.com>
2025-09-21 16:13:08 +0530

    SQL

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -16,8 +16,8 @@ limit 1 offset 1;
 -- 3) By using window function
 
 SELECT salary FROM(
-    select salary, DENSE_RANK() over (order by salary)
-) as rank where rank =2;
+    select salary, DENSE_RANK() over (order by salary DESC) as rnk from salaries
+) ranked where rnk =2;
 
 -- 4) with cte
 
@@ -27,3 +27,22 @@ with salary_rank as(
     from salaries s
 ) select * from salary_rank 
 where rnk = 2 ;
+
+
+-- 2. find duplicate record in the table
+
+--1) using group by and having
+select emp_no, count(*) as cnt from salaries group by emp_no having count(*)>1;
+
+--2) using cte and row number
+with duplicate_rec as(
+    select *, row_number() OVER (partition by emp_no order by emp_no) as rnk from salaries
+) select * from duplicate_rec where rnk>1;
+
+-- if we want to delete the duplicate rows
+with duplicate_rec as (
+    select *, row_number() over (partition by emp_no order by emp_no) as rnk from salaries
+) delete from salaries
+where emp_no in (
+    select emp_no from duplicate_rec where rnk>1
+);
