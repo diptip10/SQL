007506f0cee1a0914492754be135a75f9afdab3b
diptip10 <diptipawar811@gmail.com>
2026-02-05 22:38:37 +0530

    WHERE clause with operators

--- SQL_notes/3.Filtering.sql
+++ SQL_notes/3.Filtering.sql
@@ -83,3 +83,80 @@ SELECT *
  FROM population
 WHERE total_population > 1000000000
     OR total_population < 200000000;
+
+-- AND and OR OPERATOR TOGETHER
+-- write a SELECT statement to output all the columns from the table population that satisfies the following conditions
+-- 1. continent is Americas
+-- 2. total_population is either greater than 300 million or less than 200 million
+SELECT * 
+ FROM population
+WHERE continent = 'Americas'
+    AND (total_population > 300000000 
+    OR total_population < 200000000);
+
+-- WHERE - NEGATED CONDITIONS
+-- select statement to get the output of countries other than 'India' and 'China'
+SELECT *
+ FROM population
+WHERE country NOT IN ('india', 'China');
+
+-- fetch all columns from population table where country names do not starts with 'I' 
+SELECT *
+ FROM population
+WHERE country NOT LIKE 'I%';
+
+-- select records having the total population less than 200 billion and greater than 300 billion
+SELECT *
+ FROM population
+WHERE total_population NOT BETWEEN 200000000 AND 300000000;
+
+--  write a SELECT statement to output all the records from the table population  where the continent is NOT in Asia or Europe.
+SELECT * 
+ FROM population
+WHERE continent NOT IN ('Asia', 'Europe');
+
+-- FILTERS IN DELETE AND UPDATE
+-- update table population with new total_population of country 'United States'
+UPDATE population
+    SET total_population = 340000000
+WHERE country = 'United States';
+
+
+-- delete the records from the table where total_population is less than 1 billion(1000000000)
+DELETE
+    FROM population
+WHERE total_population < 1000000000;
+
+-- Update the population of Brazil to 220 million 
+UPDATE population
+    SET total_population = 220000000
+WHERE country = 'Brazil';
+
+-- Delete the records for countries with population less than 200 million  */
+DELETE
+    FROM population
+WHERE total_population < 200000000;
+
+-- Write a SELECT statement to query the table popular_databases and produce a result set that satisfies the following conditions.
+-- 1. database_name either starts with the string 'SQL' or ends with the string 'SQL' 
+-- 2. number_of_votes is less than 32,000
+-- Result set should include the columns database_name and number_of_votes
+SELECT database_name,
+    number_of_votes
+ FROM popular_databases
+WHERE (database_name LIKE 'SQL%' OR 
+database_name LIKE '%SQL')
+    AND number_of_votes < 32000;
+
+-- Write a SELECT statement to query the table popular_databases and produce a result set that satisfies the following conditions.
+-- database_name is not 'MySQL' or 'Microsoft SQL Server'
+-- number_of_votes is greater than or equal to 20000 and less than or equal to 30000
+-- Result set should include the columns database_name and number_of_votes
+SELECT database_name,
+    number_of_votes
+ FROM popular_databases
+WHERE (database_name <> 'MySQL' 
+    OR database_name <> 'Microsoft SQL Server')
+AND (number_of_votes >= 20000
+    AND number_of_votes <= 30000);  
+
