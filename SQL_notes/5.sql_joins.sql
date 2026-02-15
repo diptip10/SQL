48f013b65dd85a6d5ef1bcc41c3ae094c13376ef
diptip10 <diptipawar811@gmail.com>
2026-02-15 15:12:25 +0530

    join queries

--- SQL_notes/5.sql_joins.sql
+++ SQL_notes/5.sql_joins.sql
@@ -194,3 +194,68 @@ SELECT co.name AS continent_name,
        cy.population AS country_population
  FROM continent co JOIN country cy ON co.name = cy.cont_name
 WHERE cy.population  < 200000000;
+
+-- OUTER JOIN WITH FILTERS
+SELECT *
+ FROM continent co LEFT JOIN country cy ON co.name = cy.cont_name
+WHERE cont_name = 'Asia';
+
+/*
+write a SELECT statement to join the tables continent  and country using the continent name, and output the following columns as shown below.
+
+The result set should include all the records from continent table, but only the matching records from the country table. Also, the result set should only include countries with a population of less than 200 million.
+
+continent_name  - Name of the continent from either of the tables
+continent_population  - population of the continent from continent table
+country_name - Name of the country from the country table
+country_population - population of the country from the country table
+*/
+SELECT co.name AS continent_name,
+       co.population AS continent_population,
+       cy.name AS country_name,
+       cy.population AS country_population
+ FROM continent co LEFT JOIN country cy ON co.name = cy.cont_name AND cy.population < 200000000;
+
+/*
+ write a SELECT statement to join the tables continent  and country using the continent name, and output the following columns as shown below.
+
+The result set should include all the records from country table, but only the matching records from the continent table. Also, the result set should only include continents with a population of more than 1 billion.
+
+continent_name  - Name of the continent from either of the tables
+continent_population  - population of the continent from continent table
+country_name - Name of the country from the country table
+country_population - population of the country from the country table
+*/
+SELECT co.name AS continent_name,
+       co.population AS continent_population,
+       cy.name AS country_name,
+       cy.population AS country_population
+ FROM continent co RIGHT JOIN country cy ON co.name = cy.cont_name AND 
+ co.population > 1000000000;
+ 
+/*
+table - player
+columns - player_id , first_name, last_name, country and atp_ranking 
+
+table - match_result 
+columns - year, tournament, winner_id and runner_up_id
+
+Relationship - winner_id and runner_up_id in the match_result table relates to the payer table via the player_id
+
+write a SELECT statement to join the tables player  and match_result using player_id/ winner_id, and output the following columns as shown below.
+
+player_name  - Full name of the player, i.e., first_name and last_name concatenated using a blank space between them.
+
+player_country 
+
+number_of_wins - Number of wins by this player in the data provides (i.e., 2022 and 2023).
+
+Note - The output is sorted by the descending order of the number_of_wins.
+*/
+SELECT p.first_name || ' ' || p.last_name AS player_name,
+       p.country AS player_country,
+       COUNT(*) AS number_of_wins
+ FROM match_result m JOIN player p ON m.winner_id = p.player_id
+WHERE m.year IN (2022,2023)
+GROUP BY p.first_name, p.last_name, p.country 
+ORDER BY number_of_wins DESC;
