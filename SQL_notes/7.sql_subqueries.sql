68c3cac3ccdc90e15b9b4b6ebc52cdd109c7d153
diptip10 <diptipawar811@gmail.com>
2026-02-22 00:12:50 +0530

    subqueries

--- SQL_notes/7.sql_subqueries.sql
+++ SQL_notes/7.sql_subqueries.sql
@@ -32,3 +32,35 @@ WHERE continent IN (SELECT continent
                     GROUP BY continent
                     HAVING COUNT(*) > 1);
 
+/*
+write a SELECT statement to query the table population , identify the lowest populated country in each continent, and produce the result set
+*/
+
+SELECT country,
+       continent,
+       total_population
+ FROM population
+ WHERE (continent, total_population) IN (SELECT continent,
+                                                MIN(total_population)
+                                         FROM population
+                                        GROUP BY continent);
+
+
+/*
+ write a SELECT statement to query the table population , and filter the result set to include
+
+only the countries in Americas and Africa,
+
+and only the countries with the population of over 200 million
+*/
+
+SELECT country,
+       continent,
+       total_population
+ FROM (SELECT country,
+              continent,
+              total_population
+        FROM population
+       WHERE continent IN ('Americas', 'Africa'))
+WHERE total_population > 200000000;
+
