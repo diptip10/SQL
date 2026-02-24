a6fd6825e8f153f085d55bc9f02b70cb22261ba2
diptip10 <diptipawar811@gmail.com>
2026-02-24 23:21:27 +0530

    multiple cte in query

--- SQL_notes/8.sql_cte.sql
+++ SQL_notes/8.sql_cte.sql
@@ -29,3 +29,34 @@ SELECT cnty.country,
  FROM Population cnty JOIN cte_cont cont
 ON (cnty.continent = cont.continent);
        
+ /*
+  write a SELECT statement using Common Table Expressions (CTE) to query the table population, and produce the result set as shown in the screenshot below.
+
+country
+
+continent
+
+cnty_population - Population of the country
+
+highest_cont_population - Population of the highest populated country in the continent
+
+highest_world_population - Population of the highest populated country in the entire table
+*/
+
+WITH cte_continent AS 
+ (SELECT continent,
+        MAX(total_population) AS highest_cont_population
+  FROM Population
+ GROUP BY continent),
+  cte_world AS 
+  (SELECT MAX(total_population) AS highest_world_population
+    FROM Population)
+SELECT cnty.country,
+       cnty.continent,
+       cnty.total_population AS cnty_population,
+       cont.highest_cont_population,
+       cte_world.highest_world_population
+ FROM Population cnty
+JOIN cte_continent cont ON (cnty.continent = cont.continent)
+JOIN cte_world;
+
