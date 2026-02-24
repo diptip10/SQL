f56f1873ae599393219d485325911aa3c839c8a8
diptip10 <diptipawar811@gmail.com>
2026-02-24 23:09:28 +0530

    sql cte

---
+++ SQL_notes/8.sql_cte.sql
@@ -0,0 +1,31 @@
+-- CTEs -- COMMON TABLE EXPRESSIONS are mainly used to replace subqueries to make the SQL more readable 
+-- and avoid  duplication of code.
+
+/*
+write a SELECT statement using Common Table Expressions (CTE) to query the table population, and produce the result set as shown in the screenshot below.
+
+country
+
+continent
+
+cnty_population - Population of the country
+
+highest_cont_population - Population of the highest populated country in the continent
+
+lowest_cont_population - Population of the lowest populated country in the continent
+*/
+
+WITH cte_cont AS 
+ (SELECT continent,
+        MAX(total_population) AS highest_cont_population,
+        MIN(total_population) AS lowest_cont_population
+  FROM Population  
+ GROUP BY continent)
+SELECT cnty.country,
+       cnty.continent,
+       cnty.total_population AS cnty_population,
+       cont.highest_cont_population,
+       cont.lowest_cont_population
+ FROM Population cnty JOIN cte_cont cont
+ON (cnty.continent = cont.continent);
+       
