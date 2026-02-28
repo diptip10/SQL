4e71fd83772b5072fcbaf94d0bf607d26a4fc83a
diptip10 <diptipawar811@gmail.com>
2026-02-28 23:01:55 +0530

    view to simplify queries/cte

--- SQL_notes/9.sql_view.sql
+++ SQL_notes/9.sql_view.sql
@@ -14,3 +14,35 @@ SELECT continent AS name,
  FROM population
 GROUP BY continent;
 
+-- 1. Create a view with continent name, the population of the highest populated country in the continent and the population of the lowest populated country in the continent
+CREATE VIEW vw_continent
+AS 
+SELECT continent,
+       MAX(total_population) AS highest_cont_population,
+       MIN(total_population) AS lowest_cont_population
+ FROM population
+GROUP BY continent;
+
+/*
+-- 2.
+write a SELECT statement to query the table population, and produce the result set as shown in the screenshot below.
+
+country
+
+continent
+
+cnty_population - Population of the country
+
+highest_cont_population - Population of the highest populated country in the continent
+
+lowest_cont_population - Population of the lowest populated country in the continent
+*/
+
+SELECT p.country,
+       p.continent,
+       p.total_population AS cnty_population,
+       v.highest_cont_population,
+       v.lowest_cont_population
+ FROM Population p JOIN vw_continent v ON (p.continent = v.continent);
+ 
+ 
